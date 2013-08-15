36894de53801e687ac3c0906ce38ea81c00bfcc7
chinajtnet <1040058145@qq.com>
2013-08-15 17:13:49 +0800

    update

--- zTouch.js
+++ zTouch.js
@@ -2,7 +2,6 @@ function Swipe(tBox,args) {
 	var tPoint={
 		self:tBox,
 		count:0,
-		total:tBox.children().children().length,
 		speed:300,
 		iniL:30,
 		iniT:30,
@@ -10,15 +9,25 @@ function Swipe(tBox,args) {
 		touch:false,
 		mutiTouch:false,
 		setAttr:function(name,value){
-				tPoint[name]=value;
-			}
+			tPoint[name]=value;
+		}
 	};
 	
-	//初始化
-	var _this=tPoint.self;
-	for(var o in args){
-		tPoint[o]=args[o];
-	}
+	// init初始化
+	(function(){	
+		_this=tPoint.self;
+		for(var o in args){
+			tPoint[o]=args[o];
+		}
+		var _offset=_this.offset();
+		tPoint.bL=_offset.left;
+		tPoint.bT=_offset.top;
+		tPoint.bW=_this.width();
+		tPoint.bH=_this.height();
+		tPoint.bRb=tPoint.bL+tPoint.bW; //右边界
+		tPoint.bBb=tPoint.bT+tPoint.bH; //下边界
+		tPoint.total=_this.children().children().length;		
+	})();
 	
   	// 浏览器特性检测
   	tPoint.vendor = (/webkit/i).test(navigator.appVersion) ? 'webkit' :
@@ -28,7 +37,7 @@ function Swipe(tBox,args) {
   	tPoint.hasTouch = 'ontouchstart' in window;
   	tPoint.hasTransform = tPoint.vendor + 'Transform' in document.documentElement.style;
 	
-	//方向检测(左移距离，上移距离)
+	// 方向检测(左移距离，上移距离)
 	function directionDetect(l,t){
 		if(Math.abs(t)<tPoint.iniT&&Math.abs(l)>tPoint.iniL){
 			var rStr=l<0?'left':'right';
@@ -37,22 +46,22 @@ function Swipe(tBox,args) {
 		return false;
 	}
 	
-	//边界检测
+	// 边界检测
 	function borderDetect(x,y){
 		return (x<tPoint.bL||x>tPoint.bRb||y<tPoint.bT||y>tPoint.bBb);	
 	}
 	
-	//获取夹角(通过arctant反三角函数)
+	// 获取夹角(通过arctant反三角函数)
 	function getAngle(n){
 		return Math.atan(n)*180/Math.PI;
 	}
 	
-	//获取距离(通过两边计算第三边)
+	// 获取距离(通过两边计算第三边)
 	function getDis(xLen,yLen){
 		return Math.sqrt(Math.pow(xLen,2)+Math.pow(yLen,2));
 	}
 
-	//设置tPoint的改变数据（默认设置改变数据，如果setList存在，则遍历setList设置属性）
+	// 设置tPoint的改变数据（默认设置改变数据，如果setList存在，则遍历setList设置属性）
 	function setPointData(point,setList){
 		if(setList){
 			for(var o in setList){
@@ -71,7 +80,7 @@ function Swipe(tBox,args) {
 		}
 	}
 	
-	//多点触摸检测(只检测两点触摸)
+	// 多点触摸检测(只检测两点触摸)
 	function multiTouchDetect(e){
 		tPoint.tLen=e.touches.length;
 		if(tPoint.tLen>1){
@@ -98,22 +107,11 @@ function Swipe(tBox,args) {
 	
 	function startFun(e){
 		var point = e.touches[0],
-			_offset=_this.offset(),
-			bL=_offset.left,
-			bT=_offset.top,
-			bW=_this.width(),
-			bH=_this.height(),
 			t = new Date();
 		var setList={
 			startX:point.pageX,
 			startY:point.pageY,
 			startTime:t.getTime(),
-			bL:bL,
-			bT:bT,
-			bW:bW,
-			bH:bH,
-			bRb:bL+bW, //右边界
-			bBb:bT+bH, //下边界
 			identifier:point.identifier
 		}
 		setPointData(point,setList);
@@ -149,18 +147,18 @@ function Swipe(tBox,args) {
 		}
 	}
 
-	//实例化之前的回调函数
+	// 实例化之前的回调函数
 	if(typeof tPoint.beforeCallback=="function"){
 		tPoint.beforeCallback(tPoint);
 	}
 	
-	//Touch事件监听
+	// Touch事件监听
 	_this.die("touchstart,touchmove,touchend");
 	_this.get(0).addEventListener('touchstart',startFun);
 	_this.get(0).addEventListener('touchmove',moveFun);
 	_this.get(0).addEventListener('touchend',endFun);
 	
-	//实例化之后的回调函数
+	// 实例化之后的回调函数
 	if(typeof tPoint.afterCallback=="function"){
 		tPoint.afterCallback(tPoint);
 	}	
